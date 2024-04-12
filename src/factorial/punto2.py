2b27b5245da1838719f6cf83f7a774bca77179e8
estanislaopinon <estanislaopinon@gmail.com>
2024-04-11 22:25:45 -0300

    documentacion_pto2

--- src/factorial/punto2.py
+++ src/factorial/punto2.py
@@ -1,7 +1,25 @@
+# Importamos Singletonmeta desde el modulo singleton
 from singleton import SingletonMeta
 
 
 class Singleton_Calculadora(metaclass=SingletonMeta):
-
+    # Metodo para calcular la suma total de impuestos
     def Calculadora(self, base):
-        pass
+        # Calculamos IVA, impuesto a los ingresos brutos (IIBB)
+        # y las contribucines municipales
+        iva = base*0.21
+        iibb = base*0.05
+        contrib_municipales = base*0.012
+        # calculamos el total de impuestos
+        total = iva+iibb+contrib_municipales
+        return total
+
+
+if __name__ == '__main__':
+    # Creamos dos instancias Songletton_Calculadora
+    s1 = Singleton_Calculadora()
+    s2 = Singleton_Calculadora()
+    # Definimos base sobre la cual calculamos los impuestos
+    base = 120
+    # Mostramos la suma total de los impuestos
+    print(f'la suma del total de los impuestos es: {s1.Calculadora(base)}')
